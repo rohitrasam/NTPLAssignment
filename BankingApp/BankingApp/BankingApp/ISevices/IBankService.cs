adee23fae8a771e93954e021a381242e6d2b82fd
Rohit.Rasam <Rohit.Rasam@Neudesic.com>
2021-08-23 20:26:54 +0530

    added IService

--- BankingApp/BankingApp/BankingApp/ISevices/IBankService.cs
+++ BankingApp/BankingApp/BankingApp/ISevices/IBankService.cs
@@ -7,7 +7,6 @@ namespace BankingApp.ISevices
     public interface IBankService
     {
         bool AddCustomer(Customer customer);
-        //Customer FindCustomer(int accountNumber);
         bool Deposit(int accountNumber, double amount);
         bool Withdraw(int accountNumber, double amount);
         void ShowAllTransactions(int accountNumber);
